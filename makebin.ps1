fbad8b5fbafa483b89e9bc561725ee2d80294139
kian yamamoto <kianjyamamoto@gmail.com>
2020-08-28 18:45:26 -0500

    azure pipline

---
+++ makebin.ps1
@@ -0,0 +1,19 @@
+choco install ffmpeg --no-progress --version=4.2.3
+mkdir bin
+      $addPath = 'Bin'
+      Function Add-PathVariable {
+          param (
+              [string]$addPath
+          )
+          if (Test-Path $addPath){
+              $regexAddPath = [regex]::Escape($addPath)
+              $arrPath = $env:Path -split ';' | Where-Object {$_ -notMatch 
+      "^$regexAddPath\\?"}
+              $env:Path = ($arrPath + $addPath) -join ';'
+          } else {
+              Throw "'$addPath' is not a valid path."
+          }
+      }
+cd bin
+Invoke-WebRequest https://github.com/moiamond/docker-ffmpeg-base-windowsservercore/raw/master/System32/avicap32.dll -O avicap32.dll
+Invoke-WebRequest https://github.com/moiamond/docker-ffmpeg-base-windowsservercore/raw/master/System32/msvfw32.dll -O msvfw32.dll

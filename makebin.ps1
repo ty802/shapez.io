511b1bf96609fe57f10c099263dd9c0682b12a21
kian yamamoto <kianjyamamoto@gmail.com>
2020-08-28 20:10:40 -0500

    working build i hope

--- makebin.ps1
+++ makebin.ps1
@@ -14,13 +14,15 @@ mkdir bin
               Throw "'$addPath' is not a valid path."
           }
       }
+      Add-PathVariable(C:\ProgramData\chocolatey\lib\ffmpeg\tools\ffmpeg\bin)
 cd bin
 Invoke-WebRequest https://github.com/moiamond/docker-ffmpeg-base-windowsservercore/raw/master/System32/avicap32.dll -o avicap32.dll
 Invoke-WebRequest https://github.com/moiamond/docker-ffmpeg-base-windowsservercore/raw/master/System32/msvfw32.dll -o msvfw32.dll
-ls .\
-echo atempting to start prowershell as admin
+echo "atempting to start prowershell as admin"
 Start-Process -FilePath powershell.exe -ArgumentList {
-    cd c:\Windows\System32\
+cd c:\Windows\System32\
 Invoke-WebRequest https://github.com/moiamond/docker-ffmpeg-base-windowsservercore/raw/master/System32/avicap32.dll -o avicap32.dll
+echo "moved loaed avicap32.dll"
 Invoke-WebRequest https://github.com/moiamond/docker-ffmpeg-base-windowsservercore/raw/master/System32/msvfw32.dll -o msvfw32.dll
+echo "moved loaed msvfw32.dll"
 } -verb RunAs
